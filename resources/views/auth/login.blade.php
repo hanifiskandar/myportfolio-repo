358a2dd2fe123952730b3199832ef30c17981343
hanifiskandar <89310338+hanifiskandar@users.noreply.github.com>
2022-10-21 22:38:34 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,10 +31,9 @@
                         <div class="text-center mt-4">
                             <div class="mb-3">
                                 <a href="index.html" class="auth-logo">
-                                    <img src="assets/images/nep-bro.png" height="30" class="logo-dark mx-auto" alt="">
-                                    <img src="assets/images/nep-bro.png" height="30" class="logo-light mx-auto" alt="">
+                                    <img src="assets/images/logo-dark.png" height="30" class="logo-dark mx-auto" alt="">
+                                    <img src="assets/images/logo-light.png" height="30" class="logo-light mx-auto" alt="">
 
-                                    {{-- <img src="assets/images/logo-light.png" height="30" class="logo-light mx-auto" alt=""> --}}
                                 </a>
                             </div>
                         </div>
